342ea14abbd422b80cc589440caaa702feabd5a8
bz0 <bzz0217@yahoo.co.jp>
2024-10-31 21:56:03 +0900

    処理を分割

--- src/sheetExistChecker.test.ts
+++ src/sheetExistChecker.test.ts
@@ -4,10 +4,13 @@ import { CONSTANTS, ERROR_MESSAGE } from "./constants";
 
 // グローバルオブジェクトのモック
 const mockSpreadsheet = {
-  getSheetByName: jest.fn(),
+  getSheetByName: jest.fn().mockReturnThis(),
   getLastRow: jest.fn().mockReturnValue(2),
   getRange: jest.fn().mockReturnThis(),
   getValues: jest.fn().mockReturnValue([["Sheet1"], ["Sheet2"]]),
+  setValue: jest.fn(),
+  setBackground: jest.fn(),
+  isSheetHidden: jest.fn().mockReturnValue(false),
 };
 
 global.SpreadsheetApp = {
@@ -23,48 +26,39 @@ describe("sheetExistChecker", () => {
     jest.clearAllMocks(); // 全てのモックのクリア
   });
 
-  describe.only("異常処理", () => {
+  describe("異常処理", () => {
     it("シートが存在しない場合、エラーメッセージを返すこと", () => {
         mockSpreadsheet.getSheetByName.mockReturnValueOnce(null); // シートが存在しない場合
         const result = sheetExistChecker();
-        expect(result?.errorMessage).toBe(ERROR_MESSAGE.SHEET_FETCH_FAILURE_MESSAGE);
+        expect(result).toStrictEqual({errorMessage: ERROR_MESSAGE.SHEET_FETCH_FAILURE_MESSAGE});
     });
     
     it("シートが存在し行が空の場合、エラーメッセージを返すこと", () => {
         mockSpreadsheet.getSheetByName.mockReturnValueOnce(mockSpreadsheet);
         mockSpreadsheet.getLastRow.mockReturnValue(0); // データがない場合
         const result = sheetExistChecker();
-        expect(result?.errorMessage).toBe(ERROR_MESSAGE.SHEET_NAME_LIST_EMPTY_MESSAGE);
+        expect(result).toStrictEqual({errorMessage: ERROR_MESSAGE.SHEET_NAME_LIST_EMPTY_MESSAGE});
     });
   })
 
-  it("シートが存在し、データがある場合、シートチェックが行われること", () => {
-    mockSpreadsheet.getSheetByName.mockReturnValueOnce(mockSpreadsheet); // シートが存在する場合
-    mockSpreadsheet.getValues.mockReturnValue([["Sheet1"], ["Sheet2"]]); // シート名が2つ存在
+  describe("正常処理", () => {
+    it("シートが存在し、データがある場合、シートチェックが行われること", () => {
+        mockSpreadsheet.getSheetByName.mockReturnValueOnce(mockSpreadsheet); // シートが存在する場合
+        mockSpreadsheet.getValues.mockReturnValue([["Sheet1"], ["Sheet2"]]); // シート名が2つ存在
 
-    sheetExistChecker();
-
-    expect(Logger.log).toHaveBeenCalledWith("lastRowNum:1");
-    expect(mockSpreadsheet.getRange).toHaveBeenCalledWith(
-      CONSTANTS.START_COLUMN_NUM,
-      CONSTANTS.CHECK_ROW_NUM,
-      1
-    );
-  });
-
-  it("存在しないシートの場合、警告メッセージを設定すること", () => {
-    mockSpreadsheet.getSheetByName.mockImplementation((name: string) => {
-      if (name === "Sheet1") return null; // Sheet1は存在しない
-      return mockSpreadsheet; // 他のシートは存在する
+        const result = sheetExistChecker();
+        expect(result).toStrictEqual({sheetNameList: [["Sheet1"], ["Sheet2"]], acceptSheetNameList: ["Sheet1", "Sheet2"]});
     });
+    
+    it("存在しないシートの場合、警告メッセージを設定すること", () => {
+        mockSpreadsheet.getSheetByName.mockImplementation((name: string) => {
+            if (name === "Sheet1") return null; // Sheet1は存在しない
+            return mockSpreadsheet; // 他のシートは存在する
+        });
 
-    sheetExistChecker();
-
-    expect(Logger.log).toHaveBeenCalledWith("存在しないシート：Sheet1");
-    expect(setResult).toHaveBeenCalledWith({
-      message: CONSTANTS.WARNING_MESSAGE,
-      color: CONSTANTS.WARNING_COLOR,
-      checkLineNum: CONSTANTS.START_COLUMN_NUM,
+        sheetExistChecker();
+        const result = sheetExistChecker();
+        expect(result).toStrictEqual({sheetNameList: [["Sheet1"], ["Sheet2"]], acceptSheetNameList: ["Sheet2"]});
     });
-  });
+  })
 });
--- src/sheetExistChecker.ts
+++ src/sheetExistChecker.ts
@@ -7,65 +7,117 @@ type Args = {
 }
 
 type ErrorResult = {
-  errorMessage: string
+  errorMessage?: string
 }
 
-export let existsCheckerSheet:GoogleAppsScript.Spreadsheet.Sheet | null = null;
-export function sheetExistChecker () {
-  try {
-    const spreadSheet = SpreadsheetApp.getActive();
-    Logger.log(spreadSheet);
+type existingSheetNameList = string[];
 
-    existsCheckerSheet = spreadSheet.getSheetByName(CONSTANTS.SHEET_NAME);
-    if (!existsCheckerSheet) {
+type Result = {
+  sheetNameList?: string[][],
+  existingSheetNameList?: existingSheetNameList
+}
+
+type sheetNameList = string[][];
+
+export let existsCheckerSheet:GoogleAppsScript.Spreadsheet.Sheet | null = null;
+export let spreadSheet:GoogleAppsScript.Spreadsheet.Spreadsheet | null = null;
+
+export function sheetExistChecker () {
+  let result: ErrorResult | Result | null = null;
+  let sheetNameList: string[][];
+
+  try {
+    // スプレッドシートオブジェクトを取得
+    spreadSheet = SpreadsheetApp.getActive();
+    if (!spreadSheet) {
       throw new Error(ERROR_MESSAGE.SHEET_FETCH_FAILURE_MESSAGE);
     }
-
-    const lastRowNum:number = existsCheckerSheet.getLastRow() - 1;
-    if (lastRowNum === -1) {
-      // データが存在しないとき
-      throw new Error(ERROR_MESSAGE.SHEET_NAME_LIST_EMPTY_MESSAGE);
-    }
-
-    Logger.log("lastRowNum:" + lastRowNum);
-
-    // 存在チェックする列全て取得
-    // ※空行の場合は空の配列がセットされる
-    let sheetNameList: string[][] = existsCheckerSheet.getRange(
-      CONSTANTS.START_COLUMN_NUM, 
-      CONSTANTS.CHECK_ROW_NUM, 
-      lastRowNum
-    ).getValues();
-    Logger.log(sheetNameList);
-
-    if (sheetNameList.length === 0) {
-      throw new Error(ERROR_MESSAGE.SHEET_NAME_FETCH_FAILURE_MESSAGE);
-    }
-
-    for (const [i, [sheetName]] of sheetNameList.entries()) {
-      let isSheet = sheetName ? spreadSheet.getSheetByName(sheetName) : null;
-    
-      const checkLineNum = CONSTANTS.START_COLUMN_NUM + i;
-      const args: Args = {
-        message: "",
-        color: isSheet && !isSheet.isSheetHidden() ? null : CONSTANTS.WARNING_COLOR,
-        checkLineNum
-      };
-      
-      if (!isSheet || (isSheet && isSheet.isSheetHidden())) {
-        args.message = CONSTANTS.WARNING_MESSAGE;
-      }
-      
-      setResult(args);
-    }
+    sheetNameList = getSheetNameList(spreadSheet);
+    result = getExistingSheetNameList(sheetNameList);
   } catch (e: unknown) {
     if (e instanceof Error) {
       console.warn(e.stack);
       console.warn("エラー発生");
-      const result : ErrorResult = { errorMessage : e.message };
-      return result;
+      result = { errorMessage : e.message };
     }
   }
+
+  return result;
+}
+
+/**
+ * 存在するシート名リストを取得する
+ * @param sheetNameList 
+ * @returns 
+ */
+export function getExistingSheetNameList (sheetNameList: sheetNameList): Result {
+  let existingSheetNameList:existingSheetNameList = [];
+  if (!spreadSheet) {
+    throw new Error(ERROR_MESSAGE.SHEET_FETCH_FAILURE_MESSAGE);
+  }
+
+  for (const [i, [sheetName]] of sheetNameList.entries()) {
+    let isSheet = sheetName ? spreadSheet.getSheetByName(sheetName) : null;
+  
+    const checkLineNum = CONSTANTS.START_COLUMN_NUM + i;
+    const args: Args = {
+      message: "",
+      color: isSheet && !isSheet.isSheetHidden() ? null : CONSTANTS.WARNING_COLOR,
+      checkLineNum
+    };
+    
+    if (!isSheet || (isSheet && isSheet.isSheetHidden())) {
+      args.message = CONSTANTS.WARNING_MESSAGE;
+    } else {
+      existingSheetNameList.push(sheetName);
+    }
+    
+    setResult(args);
+  }
+
+  console.warn(sheetNameList);
+
+  return {
+    sheetNameList: sheetNameList,
+    existingSheetNameList: existingSheetNameList
+  }
+}
+
+/**
+ * シート名リストを取得する
+ * @param spreadSheet スプレッドシートオブジェクト
+ * @returns sheetNameList シート名リスト
+ */
+export function getSheetNameList (spreadSheet:GoogleAppsScript.Spreadsheet.Spreadsheet):sheetNameList {
+  let sheetNameList: string[][];
+
+  if (!spreadSheet) {
+    throw new Error(ERROR_MESSAGE.SHEET_FETCH_FAILURE_MESSAGE);
+  } 
+
+  // シートオブジェクト取得
+  existsCheckerSheet = spreadSheet.getSheetByName(CONSTANTS.SHEET_NAME);
+  if (!existsCheckerSheet) {
+    throw new Error(ERROR_MESSAGE.SHEET_FETCH_FAILURE_MESSAGE);
+  }
+
+  // シート行数取得
+  const lastRowNum:number = existsCheckerSheet.getLastRow() - 1;
+  if (lastRowNum === -1) {
+    // データが存在しないとき
+    throw new Error(ERROR_MESSAGE.SHEET_NAME_LIST_EMPTY_MESSAGE);
+  }
+
+  // 存在チェックする列全て取得
+  // ※空行の場合は空の配列がセットされる
+  sheetNameList = existsCheckerSheet.getRange(
+    CONSTANTS.START_COLUMN_NUM, 
+    CONSTANTS.CHECK_ROW_NUM, 
+    lastRowNum
+  ).getValues();
+  Logger.log(sheetNameList);
+
+  return sheetNameList;
 }
 
 /**
